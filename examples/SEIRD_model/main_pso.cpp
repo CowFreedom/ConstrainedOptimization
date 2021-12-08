a21c6789b55931e48afe25337f2a2b7294948698
CowFreedom <>
2021-12-08 17:45:48 +0100

    Additions and fixes to the SEIRD example

--- examples/SEIRD_model/main_pso.cpp
+++ examples/SEIRD_model/main_pso.cpp
@@ -9,8 +9,8 @@
 void evaluate_model(double t_start, double t_end, const co::EVarManager<co::EFloat64>& v, std::vector<co::EFloat64>& timepoints, std::vector<co::EFloat64>& data,co::ErrorCode& err){
 
 	//Set up parameters
-	std::vector<double> alpha={(double)v.get_param(0).val,5.00169e-07 };
-	std::vector<double> alpha_limits={0.5*(t_end-t_start)};
+	std::vector<double> alpha={4.74716e-07,(double)v.get_param(0).val };
+	std::vector<double> alpha_limits={50};
 	double kappa=0.903178;
 	double theta=4.14932000304998e-07;
 	double qq=3.01401;
@@ -23,7 +23,7 @@ void evaluate_model(double t_start, double t_end, const co::EVarManager<co::EFlo
 	std::vector<double> cumulated_infected;
 	std::vector<double> cumulated_exposed;	
 	ug::epi::SEIRD_VARA<std::vector<double>> seird_model(alpha, alpha_limits,kappa,theta,qq,pp);
-	seird_model.change_step_size_time(0.005);
+	seird_model.change_step_size_time(0.125);
 	auto result=seird_model.run_linear_implicit(t_start,u0,t_end,&cumulated_exposed,&cumulated_infected);
 	
 	//The output of the SEIRD Model must be filtered to the relevant classes with respect to the real life data set
@@ -45,8 +45,9 @@ void evaluate_model(double t_start, double t_end, const co::EVarManager<co::EFlo
 void print_run(std::string path, double t_start, double t_end, const co::EVarManager<co::EFloat64>& v){
 
 	//Set up parameters
-	std::vector<double> alpha={(double)v.get_param(0).val,5.00169e-07 };
-	std::vector<double> alpha_limits={50,100};
+	std::vector<double> alpha={4.74716e-07,(double)v.get_param(0).val };
+	//std::vector<double> alpha={4.74716e-07,5.74716e-07,1.1*5.74716e-07 };	
+	std::vector<double> alpha_limits={50,61};
 	double kappa=0.903178;
 	double theta=4.14932000304998e-07;
 	double qq=3.01401;
@@ -65,20 +66,23 @@ void print_run(std::string path, double t_start, double t_end, const co::EVarMan
 	
 	//The output of the SEIRD model are 5 classes. But you might not need all of them
 	//Filter accordingly
-	std::ofstream output;	
-	output.precision(15);
-	//Write outputs
-	output.open(path+"output.txt");
-	output<<"#time\tCumulative Infected\tDeaths\n";
-	std::vector<double> timepoints_temp=std::get<0>(result);
-	std::vector<double> data_unfiltered=std::get<1>(result);
 
-	ug::epi::write_data("", "_original_sim.txt", timepoints_temp, data_unfiltered,seird_model.names);
-	for (int i=0;(i*5)<data_unfiltered.size();i++){
-		output<<timepoints_temp[i];	
-		output<<"\t"<<cumulated_infected[i]<<"\n";    //Filter cumulative Infected		
-	}	
-	output.close();
+
+	std::vector<std::string> output_names={"Cumulative Infected"};
+	std::vector<double> timepoints_temp=std::get<0>(result);
+	std::vector<double> data_unfiltered=std::get<1>(result);	
+	ug::epi::write_data("", "_original_sim.txt", timepoints_temp, data_unfiltered,seird_model.names,"#");
+	ug::epi::write_data("", "sim_transformed.txt", timepoints_temp, cumulated_infected,output_names,"#");
+
+	std::vector<double> targettimes;
+
+	for (int i=timepoints_temp[0];i<=timepoints_temp[timepoints_temp.size()-1];i++){
+		targettimes.push_back(i);
+	}
+	std::vector<double> storage;
+	int cols=cumulated_infected.size()/timepoints_temp.size();
+	co::utility::tailor_array_linearly(targettimes, timepoints_temp, cumulated_infected,storage, cols);	
+	ug::epi::write_data("", "sim_transformed_filtered.txt", targettimes, storage,output_names,"#");	
 }
 
 int main(){
@@ -93,7 +97,7 @@ int main(){
 //	std::vector<co::EFloat64> bounds={co::EFloat64(0),co::EFloat64(1e-1),co::EFloat64(0),co::EFloat64(100),co::EFloat64(0.1),co::EFloat64(0.9),co::EFloat64(1),co::EFloat64(10),co::EFloat64(1),co::EFloat64(10)};
 //	std::vector<co::EFloat64> bounds={co::EFloat64(5e-7),co::EFloat64(1e-3),co::EFloat64(1e-10),co::EFloat64(1),co::EFloat64(0.4),co::EFloat64(0.9),co::EFloat64(1),co::EFloat64(10),co::EFloat64(1),co::EFloat64(10)};
 
-	std::vector<co::EFloat64> bounds={co::EFloat64(0),co::EFloat64(1e-5)};
+	std::vector<co::EFloat64> bounds={co::EFloat64(0),co::EFloat64(0.1)};
 
 
 	co::PSOOptions options;
@@ -110,5 +114,5 @@ int main(){
 		std::cout<<names[i]<<":"<<(double)estimated_parameters.get_param(i).val<<"\n";
 	}
 	
-	print_run("", 0, 130, estimated_parameters);
+	print_run("", 0, 90, estimated_parameters);
 }
--- examples/SEIRD_model/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-data=np.loadtxt("expData/Verstorbene.txt")
-
-plt.plot(data[0:,],data[1:,])
-plt.show
